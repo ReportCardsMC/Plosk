c5c87d95d0925050a8d32be4a620039f1320e5a5
Eliza <rileydatlord@gmail.com>
2021-10-19 21:56:20 -0700

    Add denied in plot expr and members in plot expr

---
+++ src/main/java/me/github/reportcardsmc/plotsk5/elements/expressions/OwnedPlotsExpr.java
@@ -0,0 +1,53 @@
+package me.github.reportcardsmc.plotsk5.elements.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import com.plotsquared.core.player.PlotPlayer;
+import com.plotsquared.core.plot.Plot;
+import me.github.reportcardsmc.plotsk5.utils.PlotSquaredUtil;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
+
+public class OwnedPlotsExpr extends SimpleExpression<String> {
+
+    static {
+        Skript.registerExpression(OwnedPlotsExpr.class, String.class, ExpressionType.COMBINED, "[plotsquared] owned plots of %offlineplayer%", "[plotsquared] %offlineplayer%['][s] owned plots");
+    }
+
+    private Expression<OfflinePlayer> playerExpression;
+    @Nullable
+    @Override
+    protected String[] get(Event e) {
+        if (playerExpression.getSingle(e) == null) return null;
+        return PlotSquaredUtil.getPlotIDs(playerExpression.getSingle(e).getUniqueId());
+    }
+
+    @Override
+    public boolean isSingle() {
+        return false;
+    }
+
+    @Override
+    public Class<? extends String> getReturnType() {
+        return String.class;
+    }
+
+    @Override
+    public String toString(@Nullable Event e, boolean debug) {
+        return "Checking owned plots of " + playerExpression.toString(e, debug);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        playerExpression = (Expression<OfflinePlayer>) exprs[0];
+        return true;
+    }
+}
--- src/main/java/me/github/reportcardsmc/plotsk5/utils/PlotSquaredUtil.java
+++ src/main/java/me/github/reportcardsmc/plotsk5/utils/PlotSquaredUtil.java
@@ -3,8 +3,13 @@ package me.github.reportcardsmc.plotsk5.utils;
 import com.plotsquared.core.api.PlotAPI;
 import com.plotsquared.core.plot.Plot;
 import com.plotsquared.core.plot.PlotId;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class PlotSquaredUtil {
 
@@ -17,6 +22,14 @@ public class PlotSquaredUtil {
             if (plot.getId().equals(plotId)) return plot;
         }
         return null;
+    }
+
+    public static String[] getPlotIDs(UUID uuid) {
+        List<String> ids = new ArrayList<>();
+        for (Plot plot : plotAPI.getAllPlots()) {
+            if (plot.getOwner().toString().equals(uuid.toString())) ids.add(plot.getId().toString());
+        }
+        return ids.toArray(new String[0]);
     }
 
 }
