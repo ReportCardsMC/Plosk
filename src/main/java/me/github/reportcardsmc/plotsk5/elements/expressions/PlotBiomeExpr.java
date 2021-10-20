4cb89781c85123ce5a8f0a967a7fa47048f317e3
Eliza <rileydatlord@gmail.com>
2021-10-19 22:01:45 -0700

    Add plot biome/rating expressions and add worldedit dependency

---
+++ src/main/java/me/github/reportcardsmc/plotsk5/elements/expressions/PlotBiomeExpr.java
@@ -0,0 +1,52 @@
+package me.github.reportcardsmc.plotsk5.elements.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import com.plotsquared.core.plot.Plot;
+import me.github.reportcardsmc.plotsk5.utils.PlotSquaredUtil;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Consumer;
+
+public class PlotBiomeExpr extends SimpleExpression<String> {
+    static {
+        Skript.registerExpression(PlotBiomeExpr.class, String.class, ExpressionType.COMBINED, "[PlotSquared] [the] biome of plot [with id] %string%");
+    }
+
+    private Expression<String> id;
+
+    @Nullable
+    @Override
+    protected String[] get(Event e) {
+        Plot plot;
+        if (id.getSingle(e) == null || (plot = PlotSquaredUtil.getPlot(id.getSingle(e))) == null) return null;
+        return new String[]{plot.getBiomeSynchronous().toString()};
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @Override
+    public Class<? extends String> getReturnType() {
+        return String.class;
+    }
+
+    @Override
+    public String toString(@Nullable Event e, boolean debug) {
+        return "Biome of plot: " + id.toString(e, debug);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        id = (Expression<String>) exprs[0];
+        return true;
+    }
+}
--- src/main/java/me/github/reportcardsmc/plotsk5/utils/PlotSquaredUtil.java
+++ src/main/java/me/github/reportcardsmc/plotsk5/utils/PlotSquaredUtil.java
@@ -3,10 +3,8 @@ package me.github.reportcardsmc.plotsk5.utils;
 import com.plotsquared.core.api.PlotAPI;
 import com.plotsquared.core.plot.Plot;
 import com.plotsquared.core.plot.PlotId;
-import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
