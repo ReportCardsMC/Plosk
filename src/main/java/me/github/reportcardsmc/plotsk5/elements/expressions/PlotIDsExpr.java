7770efd767de342c90bcced48c90ca835395c58f
Eliza <rileydatlord@gmail.com>
2021-10-20 13:34:02 -0700

    Fix plot flags expr, and plot ids expr

--- src/main/java/me/github/reportcardsmc/plotsk5/elements/expressions/PlotIDsExpr.java
+++ src/main/java/me/github/reportcardsmc/plotsk5/elements/expressions/PlotIDsExpr.java
@@ -7,25 +7,32 @@ import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
 import com.plotsquared.core.plot.Plot;
-import com.plotsquared.core.plot.flag.PlotFlag;
 import me.github.reportcardsmc.plotsk5.utils.PlotSquaredUtil;
+import org.bukkit.World;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
-public class PlotFlagsExpr extends SimpleExpression<String> {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class PlotIDsExpr extends SimpleExpression<String> {
+
     static {
-        Skript.registerExpression(PlotFlagsExpr.class, String.class, ExpressionType.COMBINED, "[PlotSquared] [value of] [all] flags (of|in|for) plot [with id] %string%");
+        Skript.registerExpression(PlotIDsExpr.class, String.class, ExpressionType.SIMPLE, "[plotsquared] all [of the] plot ids [in %-world%]");
     }
 
-    private Expression<String> id;
-
+    private Expression<World> worldExpression;
     @Nullable
     @Override
     protected String[] get(Event e) {
-        Plot plot;
-        if (id.getSingle(e) == null | (plot = PlotSquaredUtil.getPlot(id.getSingle(e))) == null)
-            return null;
-        return plot.getFlags().stream().map(PlotFlag::getName).toArray(String[]::new);
+        World world;
+        if (worldExpression != null) {
+            world = worldExpression.getSingle(e);
+            if (world != null) return PlotSquaredUtil.plotAPI.getAllPlots().stream().filter((p) -> Objects.equals(p.getWorldName(), world.getName())).map((p) -> p.getId().toString()).toArray(String[]::new);
+        }
+        return PlotSquaredUtil.plotAPI.getAllPlots().stream().map((p) -> p.getId().toString()).toArray(String[]::new);
     }
 
     @Override
@@ -40,13 +47,13 @@ public class PlotFlagsExpr extends SimpleExpression<String> {
 
     @Override
     public String toString(@Nullable Event e, boolean debug) {
-        return "All Flags of plot: " + id.toString(e, debug);
+        return "Get all plot ids in server";
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
-        id = (Expression<String>) exprs[0];
+        if (exprs.length != 0) worldExpression = (Expression<World>) exprs[0];
         return true;
     }
 }
